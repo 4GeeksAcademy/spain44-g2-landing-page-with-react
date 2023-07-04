b684ef870c10a87632d7e70c9d4d21885a7f6b52
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2023-07-04 15:52:16 +0000

    Cards v1.0

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import { Cards } from "./component/Cards";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
