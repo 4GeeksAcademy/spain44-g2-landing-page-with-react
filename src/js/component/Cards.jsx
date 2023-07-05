2d4ecf321103bb68834099645c636e06f7647ad3
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2023-07-05 15:35:03 +0000

    Parte Cards

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -1,36 +1,38 @@
 import React from "react";
+import cardImg from "../../img/Card.jpg"
+
 
 
 export const Cards = () => {
 
     return (
         <div>
-            <div class="card-group">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div className="card-group">
+  <div className="card">
+    <img src={cardImg} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
     </div>
   </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div className="card">
+    <img src={cardImg} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
     </div>
   </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div className="card">
+    <img src={cardImg} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
     </div>
   </div>
 </div>
